5806766abc14618addfa90ecda1268719efd4633
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2019-08-25 22:34:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,25 @@
 class Person
   
   def initialize(name, job)
-    
+    @name=name
+    @job=job
+  end
+  
+  def name
+    @name 
+  end
+  
+  def name=(new_name)
+    @name=(new_name)
+  end
+
+ def job 
+   @job
+  end 
+  
+def job=(job_name)
+  @job=(job_name)
+end
   
   
 end
